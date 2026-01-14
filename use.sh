4aa50992c94fe318caf6b890c55f956834ff63f0
Gueguettee <gaetan.jenni@gmail.com>
2026-01-14 21:36:15 +0100

    Multi pairs

--- use.sh
+++ use.sh
@@ -1,2 +1,7 @@
-docker-compose run --rm passivbot python src/backtest.py strategies/hype_top.json
-docker-compose run -d passivbot python src/backtest.py strategies/hype_top.json
+# Launch backtest:
+docker compose run --rm passivbot python src/backtest.py strategies/hype_top.json
+docker compose run --rm passivbot python src/backtest.py strategies/hype_top_4pairs.json
+
+# Launch backtest in detached mode:
+docker compose run -d passivbot python src/backtest.py strategies/hype_top.json
+docker compose run -d passivbot python src/backtest.py strategies/hype_top_4pairs.json
